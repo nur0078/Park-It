316d540e136347d3ca945ad6171c6a788ee6bbe0
Slok Bhandari <slok@Sloks-MacBook-Pro.local>
2024-01-10 20:23:57 +1100

    Intial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Flex,
+  HStack,
+  IconButton,
+  Input,
+  SkeletonText,
+  Text,
+} from "@chakra-ui/react";
+import { FaLocationArrow, FaTimes } from "react-icons/fa";
+
+import { useJsApiLoader, GoogleMap } from "@react-google-maps/api";
+
+const center = { lat: -33.85672, lng: 151.21544 };
 
 function App() {
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+  });
+
+  if (!isLoaded) {
+    return <SkeletonText />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Flex
+      position="relative"
+      flexDirection="column"
+      alignItems="center"
+      h="100vh"
+      w="100vw"
+    >
+      <Box position="absolute" left={0} top={0} h="100%" w="100%">
+        {/* Google Map Box */}
+        <GoogleMap
+          center={center}
+          zoom={15}
+          mapContainerStyle={{ width: "100%", height: "100%" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {/* Displaying markers, or directions */}
+        </GoogleMap>
+      </Box>
+
+      <Box
+        p={4}
+        borderRadius="lg"
+        mt={4}
+        bgColor="white"
+        shadow="base"
+        minW="container.md"
+        zIndex="modal"
+      >
+        <HStack spacing={4}>
+          <Input type="text" placeholder="Origin" />
+          <Input type="text" placeholder="Destination" />
+          <ButtonGroup>
+            <Button colorScheme="pink" type="submit">
+              Calculate Route
+            </Button>
+            <IconButton
+              aria-label="center back"
+              icon={<FaTimes />}
+              onClick={() => alert(123)}
+            />
+          </ButtonGroup>
+        </HStack>
+        <HStack spacing={4} mt={4} justifyContent="space-between">
+          <Text>Distance: </Text>
+          <Text>Duration: </Text>
+          <IconButton
+            aria-label="center back"
+            icon={<FaLocationArrow />}
+            isRound
+            onClick={() => alert(123)}
+          />
+        </HStack>
+      </Box>
+    </Flex>
   );
 }
 
