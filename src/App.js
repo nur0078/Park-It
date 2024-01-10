d33a0ac8705e86b9c446e8bedaddfafc46977c9c
Slok Bhandari <slok@Sloks-MacBook-Pro.local>
2024-01-11 00:24:28 +1100

    made responsive

--- src/App.js
+++ src/App.js
@@ -79,94 +79,96 @@ function App() {
   }
 
   return (
-    <Flex
-      position="relative"
-      flexDirection="column"
-      alignItems="center"
-      h="100vh"
-      w="100vw"
-    >
-      <Box position="absolute" left={0} top={0} h="100%" w="100%">
-        {/* Google Map Box */}
-
-        <GoogleMap
-          center={center}
-          zoom={15}
-          mapContainerStyle={{ width: "100%", height: "100%" }}
-          options={{
-            zoomControl: false,
-            // streetViewControl: false,
-            // mapTypeControl: false,
-            fullscreenControl: false,
-          }}
-          onLoad={(map) => setMap(map)}
-        >
-          <Marker position={center} />
-
-          {/* Displaying markers, or directions */}
-
-          {directionsResponse && (
-            <DirectionsRenderer directions={directionsResponse} />
-          )}
-        </GoogleMap>
-      </Box>
-
-      <Box
-        p={4}
-        borderRadius="lg"
-        mt={4}
-        bgColor="white"
-        shadow="base"
-        minW="container.md"
-        zIndex="docked"
+    <div className="flex">
+      <Flex
+        position="relative"
+        flexDirection="column"
+        alignItems="center"
+        h="100vh"
+        w="100vw"
       >
-        <Text
-          color="black"
-          fontWeight="bold"
-          letterSpacing="wide"
-          fontSize="xl"
-          textTransform="uppercase"
-          textAlign="center"
-          margin="3"
+        <Box position="absolute" left={0} top={0} h="100%" w="100%">
+          {/* Google Map Box */}
+
+          <GoogleMap
+            center={center}
+            zoom={15}
+            mapContainerStyle={{ width: "100%", height: "100%" }}
+            options={{
+              zoomControl: false,
+              // streetViewControl: false,
+              // mapTypeControl: false,
+              fullscreenControl: false,
+            }}
+            onLoad={(map) => setMap(map)}
+          >
+            <Marker position={center} />
+
+            {/* Displaying markers, or directions */}
+
+            {directionsResponse && (
+              <DirectionsRenderer directions={directionsResponse} />
+            )}
+          </GoogleMap>
+        </Box>
+
+        <Box
+          p={4}
+          borderRadius="lg"
+          mt={4}
+          bgColor="white"
+          shadow="base"
+          maxW="container.md"
+          zIndex="docked"
         >
-          PARK IT
-        </Text>
+          <Text
+            color="black"
+            fontWeight="bold"
+            letterSpacing="wide"
+            fontSize="xl"
+            textTransform="uppercase"
+            textAlign="center"
+            margin="3"
+          >
+            PARK IT
+          </Text>
 
-        <HStack spacing={4}>
-          <Autocomplete>
-            <Input type="text" placeholder="From" ref={originRef} />
-          </Autocomplete>
+          <HStack spacing={4}>
+            <Autocomplete>
+              <Input type="text" placeholder="From" ref={originRef} />
+            </Autocomplete>
 
-          <Autocomplete>
-            <Input type="text" placeholder="To" ref={destinationRef} />
-          </Autocomplete>
+            <Autocomplete>
+              <Input type="text" placeholder="To" ref={destinationRef} />
+            </Autocomplete>
 
-          <ButtonGroup>
-            <Button colorScheme="pink" type="submit" onClick={calculateRoute}>
-              Calculate Route
-            </Button>
+            <ButtonGroup>
+              <Button colorScheme="pink" type="submit" onClick={calculateRoute}>
+                Calculate Route
+              </Button>
+              <IconButton
+                aria-label="center back"
+                icon={<FaTimes />}
+                onClick={clearRoute}
+              />
+            </ButtonGroup>
+          </HStack>
+          <HStack spacing={4} mt={4} justifyContent="space-between">
+            <Text>Distance: {distance} </Text>
+            <Text>Duration: {duration} </Text>
             <IconButton
               aria-label="center back"
-              icon={<FaTimes />}
-              onClick={clearRoute}
+              icon={<FaLocationArrow />}
+              isRound
+              onClick={() => {
+                map.panTo(center);
+                map.setZoom(15);
+              }}
             />
-          </ButtonGroup>
-        </HStack>
-        <HStack spacing={4} mt={4} justifyContent="space-between">
-          <Text>Distance: {distance} </Text>
-          <Text>Duration: {duration} </Text>
-          <IconButton
-            aria-label="center back"
-            icon={<FaLocationArrow />}
-            isRound
-            onClick={() => {
-              map.panTo(center);
-              map.setZoom(15);
-            }}
-          />
-        </HStack>
-      </Box>
-    </Flex>
+          </HStack>
+        </Box>
+      </Flex>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import { ChakraProvider, theme } from '@chakra-ui/react'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { ChakraProvider, theme } from "@chakra-ui/react";
 
-
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-     <ChakraProvider theme={theme}>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
